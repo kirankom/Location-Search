334e8a5660d5d2da461aca8f3f78ac091f8ca05c
Meet Vora <meetrajivvora@gmail.com>
2020-06-05 13:56:32 -0700

    changed parser() to return a RoaringBitmap. saved the timestamps in a times list, and the encoded lat/long in a field var writer

--- src/main/java/DatabaseWriter.java
+++ src/main/java/DatabaseWriter.java
@@ -1,44 +1,79 @@
-// import java.util.Iterator;
-// import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import org.roaringbitmap.RoaringBitmap;
+
 public class DatabaseWriter {
 
-    public static void main(String[] args) throws Exception {
+    // private final long FIRST_TIMESTAMP = ;
+
+    public static void main(String[] args) {
         DatabaseWriter dw = new DatabaseWriter();
-        dw.run();
+        dw.parser();
     }
 
-    public void run() throws Exception {
+    DatabaseWriter() {
+        _times = new ArrayList<>();
+        _writer = new StringWriter();
+        // _firstTimestamp = 0L;
+    }
+
+    public void run() {
         // Iterator iter = Parser.run(filename);
 
     }
 
-    public void parser() {
+    public RoaringBitmap parser() {
         String filename = "C:/Users/meetr/Documents/personal_projects/Location-Search/src/test/java/test.json";
 
-        JSONObject jsonObj = (JSONObject) new JSONParser().parse(new FileReader(filename));
+        // List<Long> timestamps = new ArrayList<>();
+        // StringWriter writer = new StringWriter();
 
-        JSONArray jsonArray = (JSONArray) jsonObj.get("locations");
+        try {
 
-        for (Object obj : jsonArray) {
-            JSONObject location = (JSONObject) obj;
+            JSONObject jsonObj = (JSONObject) new JSONParser().parse(new FileReader(filename));
 
-            long timestamp = Long.parseLong((String) location.get("timestampMs"));
-            System.out.println("Time = " + timestamp);
+            JSONArray jsonArray = (JSONArray) jsonObj.get("locations");
 
-            long lat = (Long) location.get("latitudeE7");
-            System.out.println("Latitude = " + lat);
+            for (Object obj : jsonArray) {
+                JSONObject location = (JSONObject) obj;
 
-            long lon = (Long) location.get("longitudeE7");
-            System.out.println("Longitude = " + lon);
-            System.out.println();
-            // String timestamp = (String) location.get("timestampMs");
+                long timestamp = Long.parseLong((String) location.get("timestampMs"));
+                System.out.println("Time = " + timestamp);
+
+                double lat = ((Long) location.get("latitudeE7") * 1.0) / 1e7;
+                System.out.println("Latitude = " + lat);
+
+                double lon = ((Long) location.get("longitudeE7") * 1.0) / 1e7;
+                System.out.println("Longitude = " + lon);
+                System.out.println();
+
+                _times.add(timestamp);
+                DataUtils.encodeLocation(lat, lon, _writer);
+
+                // String timestamp = (String) location.get("timestampMs");
+            }
+        } catch (FileNotFoundException e) {
+            System.exit(1);
+        } catch (IOException e) {
+            System.exit(1);
+        } catch (ParseException e) {
+            System.exit(1);
         }
-
+        return DataUtils.addTimestamps(_times, _times.get(0));
     }
+
+    private List<Long> _times;
+    private StringWriter _writer;
+    // private Long _firstTimestamp;
 
 }
