a5f97649510305c2338cef882954497a1f278f7f
Meet Vora <meetrajivvora@gmail.com>
2020-06-05 11:34:41 -0700

    added new test.json file, and added json dependencies to pom.xml

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,3 +1,20 @@
-public class Parser {
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class Parser {
+    public static void main() throws Exception {
+        Parser parser = new Parser();
+        String filename = "test.json";
+        parser.run(filename);
+    }
+
+    public void run(String filename) throws Exception {
+        Object obj = new JSONParser().parse(new FileReader(filename));
+        JSONObject jo = (JSONObject) obj;
+    }
 }
