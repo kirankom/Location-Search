ac052be64d2b205936627947aa08d2b86b05440a
kirankom <kiran.komaravolu@gmail.com>
2020-06-07 17:15:48 -0700

    Create ReadTest.java

---
+++ src/main/java/ReadTest.java
@@ -0,0 +1,32 @@
+package lsr_ws;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.ParseException;
+
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.exception.InvalidShapeException;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.impl.BufferedLineString;
+
+public class ReadTest {
+
+	public static void main(String[] args) throws InvalidShapeException, IOException, ParseException {
+		SpatialContextFactory factory = new SpatialContextFactory();
+		SpatialContext s = SpatialContext.GEO;
+		// TODO Auto-generated method stub
+		PolyshapeReader reader = new PolyshapeReader(s , factory);
+		
+
+		BufferedLineString shape = (BufferedLineString) reader.read("1~wj{Uezb_G") ;
+		
+		System.out.println(shape.getClass());
+		
+		shape.getPoints().forEach(point -> System.out.println("Latitude::" + point.getY() + "::Longitude" + point.getX()));
+		
+		
+	}
+
+}
