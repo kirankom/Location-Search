b67d2b287cd957aad5e126c7582ef3ebcacbe5fe
Meet Vora <meetrajivvora@gmail.com>
2020-06-11 13:38:49 -0700

    Moved parser from DatabaseWriter to DataUtils. Added database connection methods to Writer as well.

--- src/main/java/location_search/DataUtils.java
+++ src/main/java/location_search/DataUtils.java
@@ -1,16 +1,20 @@
 package location_search;
 
 import org.locationtech.spatial4j.shape.Point;
-import org.locationtech.spatial4j.shape.impl.BufferedLineString;
-import org.locationtech.spatial4j.shape.Shape;
-import org.locationtech.spatial4j.io.PolyshapeWriter.Encoder;
-import org.locationtech.spatial4j.io.PolyshapeReader;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.context.SpatialContextFactory;
-import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.shape.impl.BufferedLineString;
+import org.locationtech.spatial4j.io.PolyshapeWriter.Encoder;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+import java.io.FileReader;
 
 import org.roaringbitmap.RoaringBitmap;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import org.locationtech.spatial4j.exception.InvalidShapeException;
@@ -36,14 +40,10 @@ public class DataUtils {
      * @param lon    Longitude of user
      * @param writer StringWriter instance that stores encoded location
      */
-    public static void encodeLocation(double lat, double lon, StringWriter writer) {
+    public static void encodeLocation(double lat, double lon, StringWriter writer) throws IOException {
         // StringWriter writer = new StringWriter();
         Encoder encoder = new Encoder(writer);
-        try {
-            encoder.write(lon, lat);
-        } catch (IOException e) {
-            System.exit(1);
-        }
+        encoder.write(lon, lat);
     }
 
     /**
@@ -52,10 +52,10 @@ public class DataUtils {
      * @param compressedEncodedStr A byte array of the compressed encoded String
      * @throws InvalidShapeException
      * @throws IOException
-     * @throws ParseException
+     * @throws java.text.ParseException
      */
     public static void decodeLocation(byte[] compressedEncodedStr)
-            throws InvalidShapeException, IOException, ParseException {
+            throws InvalidShapeException, IOException, java.text.ParseException {
 
         String encodedStr = new String(compressedEncodedStr);
 
@@ -109,14 +109,45 @@ public class DataUtils {
      * 
      * @param data byte array containing serialized data
      * @return RoaringBitmap instance
+     * @throws IOException
      */
-    public static RoaringBitmap deserializeBitmap(byte[] data) {
+    public static RoaringBitmap deserializeBitmap(byte[] data) throws IOException {
         RoaringBitmap bitmap = new RoaringBitmap();
-        try {
-            bitmap.deserialize(ByteBuffer.wrap(data));
-        } catch (IOException e) {
-            System.exit(2);
-        }
+        bitmap.deserialize(ByteBuffer.wrap(data));
         return bitmap;
+    }
+
+    /**
+     * Parses each data point consisting of a timestamp, latitude, and longitude
+     * from the file _filename and stores it in the inputted instances.
+     * 
+     * @param writer StringWriter instance that stores encoded String
+     * @param times  List object containing all timestamps stored in json file
+     * @return RoaringBitmap instance that compresses timestamps
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws org.json.simple.parser.ParseException
+     */
+    public static RoaringBitmap parser(StringWriter writer, List<Long> times, String filename)
+            throws FileNotFoundException, IOException, org.json.simple.parser.ParseException {
+
+        Encoder encoder = new Encoder(writer);
+        JSONObject jsonObj = (JSONObject) new JSONParser().parse(new FileReader(filename));
+
+        JSONArray jsonArray = (JSONArray) jsonObj.get("locations");
+
+        for (Object obj : jsonArray) {
+            JSONObject location = (JSONObject) obj;
+
+            long timestamp = Long.parseLong((String) location.get("timestampMs"));
+
+            double lat = ((Long) location.get("latitudeE7") * 1.0) / 1e7;
+            double lon = ((Long) location.get("longitudeE7") * 1.0) / 1e7;
+
+            times.add(timestamp);
+            encoder.write(lon, lat);
+        }
+        // _firstTimestamp = times.get(0);
+        return DataUtils.addTimestamps(times, times.get(0));
     }
 }
--- src/main/java/location_search/DatabaseWriter.java
+++ src/main/java/location_search/DatabaseWriter.java
@@ -11,13 +11,19 @@ import java.io.ByteArrayOutputStream;
 
 import org.roaringbitmap.RoaringBitmap;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+
 import java.io.IOException;
+import java.text.ParseException;
+import java.io.FileNotFoundException;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
@@ -46,60 +52,6 @@ public class DatabaseWriter {
     }
 
     /**
-     * Parses each data point consisting of a timestamp, latitude, and longitude
-     * from the file _filename and stores it in the inputted instances.
-     * 
-     * @param writer StringWriter instance that stores encoded String
-     * @param times  List object containing all timestamps stored in json file
-     * @return RoaringBitmap instance that compresses timestamps
-     */
-    public RoaringBitmap parser(StringWriter writer, List<Long> times) {
-        try {
-            Encoder encoder = new Encoder(writer);
-            JSONObject jsonObj = (JSONObject) new JSONParser().parse(new FileReader(_filename));
-
-            JSONArray jsonArray = (JSONArray) jsonObj.get("locations");
-
-            for (Object obj : jsonArray) {
-                JSONObject location = (JSONObject) obj;
-
-                long timestamp = Long.parseLong((String) location.get("timestampMs"));
-
-                double lat = ((Long) location.get("latitudeE7") * 1.0) / 1e7;
-
-                double lon = ((Long) location.get("longitudeE7") * 1.0) / 1e7;
-
-                times.add(timestamp);
-                encoder.write(lon, lat);
-            }
-        } catch (FileNotFoundException e) {
-            System.exit(1);
-        } catch (IOException e) {
-            System.exit(1);
-        } catch (ParseException e) {
-            System.exit(1);
-        }
-        _firstTimestamp = times.get(0);
-        return DataUtils.addTimestamps(times, _firstTimestamp);
-    }
-
-    /**
-     * Adds the user ID, the first timestamp, the compressed byte array of
-     * timestamps, and the encoded latitude/longitude String into a database as one
-     * entry.
-     */
-    public void databaseAdder() {
-        String json_file = "C:/Users/meetr/Documents/personal_projects/Location-Search/src/test/test.json";
-        List<Long> times = new ArrayList<Long>();
-        StringWriter writer = new StringWriter();
-
-        RoaringBitmap bitmap = parser(writer, times);
-        byte[] timeData = DataUtils.serializeBitmap(bitmap);
-
-        byte[] compressedEncoding = convertStr(writer.toString());
-    }
-
-    /**
      * Compresses and returns given encoded String as a byte array.
      * 
      * @param encodedStr Encoded Latitude/Longitude String
@@ -115,15 +67,71 @@ public class DatabaseWriter {
             compressor.flush();
             compressor.close();
         } catch (CompressorException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.exit(1);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.exit(1);
         }
-
         // System.out.println("Compressed byte String:" + fos.size() + ":::" +
         // "Compression Type:::" + t);
         return fos.toByteArray();
+    }
+
+    /**
+     * Adds the user ID, the first timestamp, the compressed byte array of
+     * timestamps, and the encoded latitude/longitude String into a database as one
+     * entry.
+     */
+    public void databaseAdder() {
+        String json_file = "C:/Users/meetr/Documents/personal_projects/Location-Search/src/test/test.json";
+        List<Long> times = new ArrayList<Long>();
+        StringWriter writer = new StringWriter();
+
+        try {
+            RoaringBitmap bitmap = DataUtils.parser(writer, times, _filename);
+            byte[] timeData = DataUtils.serializeBitmap(bitmap);
+            byte[] compressedEncoding = convertStr(writer.toString());
+
+        } catch (FileNotFoundException e) {
+            System.exit(1);
+        } catch (IOException e) {
+            System.exit(1);
+        } catch (org.json.simple.parser.ParseException e) {
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates a data table in the connected database
+     * 
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public void createTable() throws ClassNotFoundException, SQLException {
+        String command = "CREATE TABLE IF NOT EXISTS location_data ("
+                + "user_ID int NOT null, first_timestamp bigint NOT null, timestamps VARBINARY, locations VARBINARY)";
+
+        Connection conn = establishConnection();
+        PreparedStatement create = conn.prepareStatement(command);
+        create.executeUpdate();
+    }
+
+    /**
+     * Establishes a connection with a MySQL database.
+     * 
+     * @return database connection instance
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connection establishConnection() throws ClassNotFoundException, SQLException {
+
+        String database_name = "location_search";
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/" + database_name;
+        String username = "root";
+        String password = "4quhgfzn";
+
+        Class.forName(driver);
+        Connection conn = DriverManager.getConnection(url, username, password);
+        return conn;
     }
 }
