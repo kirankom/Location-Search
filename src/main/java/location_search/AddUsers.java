d165b4690df0f7c2e13b18a6634009f821d51c82
Meet Vora <meetrajivvora@gmail.com>
2020-06-26 11:35:39 -0700

    Created new higher-level class to add a series of users

---
+++ src/main/java/location_search/AddUsers.java
@@ -0,0 +1,14 @@
+package location_search;
+
+public class AddUsers {
+
+    void run() {
+
+        DatabaseWriter dbWriter = new DatabaseWriter();
+
+        int[] userIDS = new int[] { 1, 2, 3, 4, 5 };
+        String filename = "C:/Users/meetr/Documents/personal_projects/Location-Search/src/test/test.json";
+
+    }
+
+}
