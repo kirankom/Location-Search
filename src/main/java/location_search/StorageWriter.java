679294475546ce02ac8b88dc02ad161c09b1cf0d
Meet Vora <meetrajivvora@gmail.com>
2020-06-29 17:09:01 -0700

    Added establish Connection and createtable methods to storage writer

--- src/main/java/location_search/StorageWriter.java
+++ src/main/java/location_search/StorageWriter.java
@@ -1,5 +1,81 @@
 package location_search;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class StorageWriter implements IStoreWriter {
 
+    /** Name of the database to store data in. */
+    private String _databaseName;
+
+    /** Username of database. */
+    private String _username;
+
+    /** Password of database. */
+    private String _password;
+
+    private Connection _conn;
+
+    // private Statement _tableStmt;
+
+    public StorageWriter(String databaseName, String username, String password) {
+        this._databaseName = databaseName;
+        this._username = username;
+        this._password = password;
+        this._conn = establishConnection();
+        createTable();
+    }
+
+    public StorageWriter() {
+        this("location_search", "ls", "locationSearch");
+    }
+
+    public void upsertRecord(Record record) {
+    }
+
+    public Record getRecord(long userID) {
+
+    }
+
+    public void commit() {
+    }
+
+    private void createTable() {
+        String tableCmd = "CREATE TABLE IF NOT EXISTS testing123 (user_ID INTEGER PRIMARY KEY NOT NULL, first_timestamp BIGINT NOT NULL, timestamps VARBINARY(30000) NOT NULL, locations VARBINARY(30000) NOT NULL)";
+
+        try {
+            Statement tableStmt = _conn.createStatement();
+            tableStmt.executeUpdate(tableCmd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Establishes a connection with the specified MySQL database.
+     * 
+     * @return database connection instance
+     */
+    private Connection establishConnection() {
+
+        Connection conn = null;
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/" + _databaseName;
+        // String username = "ls";
+        // String password = "locationSearch";
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, _username, _password);
+        } catch (ClassNotFoundException e) {
+            System.exit(1);
+        } catch (SQLException e) {
+            System.exit(1);
+        }
+        return conn;
+    }
 }
