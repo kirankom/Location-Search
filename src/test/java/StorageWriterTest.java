09596d7197ae2c799c7649a276d3b4af4a1c9e02
Meet Vora <meetrajivvora@gmail.com>
2020-06-29 17:48:01 -0700

    Setup new StorageWriter test file

---
+++ src/test/java/StorageWriterTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import location_search.StorageWriter;
+
+public class StorageWriterTest {
+
+    private StorageWriter sw = new StorageWriter();
+
+    @Test
+    public void testInsertCmd() {
+        System.out.println("HERE");
+    }
+
+}
