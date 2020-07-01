c3decb57ff259f13c8d53fe3610b5c54fabd537b
Meet Vora <meetrajivvora@gmail.com>
2020-06-30 20:50:14 -0700

    Added 3 records to test, which all pass

--- src/test/java/StorageWriterTest.java
+++ src/test/java/StorageWriterTest.java
@@ -8,10 +8,16 @@ public class StorageWriterTest {
 
     @Test
     public void testInsertCmd() {
-        Record record = new Record(1, 10, new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 });
-
-        sw.upsertRecord(record);
+        Record record1 = new Record(1, 7, new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 });
+        sw.upsertRecord(record1);
         // sw.commit();
+
+        Record record2 = new Record(2, 4, new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 });
+        sw.upsertRecord(record2);
+
+        Record record3 = new Record(2, 1111111, new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 });
+        sw.upsertRecord(record3);
+        sw.commit();
         System.out.println("DONE!");
     }
 
