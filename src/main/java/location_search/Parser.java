2fe9f8cdfde0357edbf05aee28345adb2ef2b5ca
Meet Vora <meetrajivvora@gmail.com>
2020-07-01 16:26:09 -0700

    Updated code in parser.java

--- src/main/java/location_search/Parser.java
+++ src/main/java/location_search/Parser.java
@@ -2,11 +2,10 @@
 
 // import java.io.FileReader;
 // import java.io.StringWriter;
+// import java.util.ArrayList;
 // import java.util.Iterator;
 // import java.util.List;
 // import java.util.Map;
-
-// import com.google.gson.JsonArray;
 
 // import org.json.simple.JSONArray;
 // import org.json.simple.JSONObject;
@@ -19,16 +18,24 @@
 // * Parses each data point consisting of a timestamp, latitude, and longitude
 // * from the file _filename and stores it in the inputted instances.
 // *
-// * @param writer StringWriter instance that stores encoded String
-// * @param times List object containing all timestamps stored in json file
+// * @param filename Name of file to parse
 // * @return RoaringBitmap instance that compresses timestamps
+// * @throws IOException
+// * @throws FileNotFoundException
+// * @throws org.json.simple.parser.ParseException
 // */
-// public static RoaringBitmap parser(StringWriter writer, List<Long> times) {
-// try {
-// Encoder encoder = new Encoder(writer);
-// JSONObject jsonObj = (JSONObject) new JSONParser().parse(new
-// FileReader(_filename));
+// public Record parse(String filename)
+// throws FileNotFoundException, IOException,
+// org.json.simple.parser.ParseException {
 
+// List<Record> records = new ArrayList<Record>();
+
+// // Encoder encoder = new Encoder(_writer);
+// JSONObject jsonObj = (JSONObject) new JSONParser().parse(new
+// FileReader(filename));
+
+// // should change "locations" to whatever the name of the list in the
+// // json file is
 // JSONArray jsonArray = (JSONArray) jsonObj.get("locations");
 
 // for (Object obj : jsonArray) {
@@ -37,20 +44,12 @@
 // long timestamp = Long.parseLong((String) location.get("timestampMs"));
 
 // double lat = ((Long) location.get("latitudeE7") * 1.0) / 1e7;
-
 // double lon = ((Long) location.get("longitudeE7") * 1.0) / 1e7;
 
-// times.add(timestamp);
-// encoder.write(lon, lat);
+// _times.add(timestamp);
+// _encoder.write(lon, lat);
 // }
-// } catch (FileNotFoundException e) {
-// System.exit(1);
-// } catch (IOException e) {
-// System.exit(1);
-// } catch (ParseException e) {
-// System.exit(1);
-// }
-// _firstTimestamp = times.get(0);
-// return DataUtils.addTimestamps(times, _firstTimestamp);
+// // _firstTimestamp = times.get(0);
+// return DataUtils.addTimestamps(_times, _times.get(0));
 // }
 // }
