086cc9d7c9726231d12885af467d9ede0978860a
Meet Vora <meetrajivvora@gmail.com>
2020-06-30 19:43:08 -0700

    Added old insertCmd to test class for reference

--- src/test/java/StorageWriterTest.java
+++ src/test/java/StorageWriterTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import location_search.StorageWriter;
+import location_search.Record;
 
 public class StorageWriterTest {
 
@@ -7,7 +8,17 @@ public class StorageWriterTest {
 
     @Test
     public void testInsertCmd() {
-        System.out.println("HERE");
+        Record record = new Record(1, 10, new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 });
+
+        sw.upsertRecord(record);
+        // sw.commit();
+        System.out.println("DONE!");
     }
 
 }
+// String other = "IF EXISTS (SELECT user_ID FROM " + _tableName + " WHERE
+// user_ID = ?) INSERT INTO " + _tableName
+// + "(user_ID, first_timestamp, timestamps, coordinates) VALUES (?, ?, ?, ?)
+// ELSE UPDATE " + _tableName
+// + " SET first_timestamp = ?, timestamps = ?, coordinates = ?" + "WHERE
+// user_ID = ?";
