e76526cfa1ffba3c810726081d5b5751d9f6d3b8
Meet Vora <meetrajivvora@gmail.com>
2020-07-04 14:23:44 -0700

    Changed the name of a variable

--- src/main/java/location_search/ICompress.java
+++ src/main/java/location_search/ICompress.java
@@ -8,17 +8,16 @@ package location_search;
  */
 public interface ICompress {
 
-    byte[] compressTimestamps(Iterable<Long> times, long firstTime);
+    byte[] compressTimestamps(Iterable<Long> times, long firstTimestamp);
 
     byte[] compressCoordinates(Iterable<Coordinate> coordiantes);
-
-    /** Add timestamps in iterable to end of byte array */
-    byte[] appendTimestamps(byte[] times, Iterable<Long> iterTimes, long firstTimestamp);
-
-    byte[] appendCoordiantes(byte[] coordinates, Iterable<Coordinate> iterCoordinates);
 
     Iterable<Long> decompressTimestamps(byte[] compressedTimes, long firstTimestamp);
 
     Iterable<Coordinate> decompressCoordinates(byte[] coordianteArray);
+
+    byte[] appendTimestamps(byte[] times, Iterable<Long> iterTimes, long firstTimestamp);
+
+    byte[] appendCoordiantes(byte[] coordinates, Iterable<Coordinate> iterCoordinates);
 
 }
